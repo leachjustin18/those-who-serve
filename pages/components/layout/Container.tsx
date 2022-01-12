9e3fe2cb9b00e88708089658ab84604afb302f4a
Justin Leach <leachjustin18@yahoo.com>
2022-01-12 17:34:55 -0600

    Story: Created base layout and reusabled components

---
+++ pages/components/layout/Container.tsx
@@ -0,0 +1,168 @@
+import { ReactElement, useState, MouseEvent } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRouter } from 'next/router';
+import {
+  AppBar,
+  Container as MuiContainer,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Box,
+  IconButton,
+  Menu,
+  Button,
+  Tooltip,
+  Avatar,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
+import firebase from '../../firebase/clientApp';
+
+const Container = ({ children }: { children: ReactElement }): ReactElement => {
+  const firebaseAuth = firebase.auth();
+  const router = useRouter();
+  const [user] = useAuthState(firebaseAuth);
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const logUserOut = () => {
+    firebaseAuth.signOut();
+    router.push('/auth');
+  };
+
+  const pages = [
+    'Servants',
+    'Adult Jobs',
+    'Kid Jobs',
+    'Upcoming Schedule',
+    'Create Schedule',
+  ];
+  const siteTitle = 'Those Who Serve';
+
+  return (
+    <>
+      <AppBar position="sticky">
+        <MuiContainer maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              {siteTitle}
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              {siteTitle}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              {user ? (
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt={user.displayName} src={user.photoURL} />
+                  </IconButton>
+                </Tooltip>
+              ) : null}
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" onClick={logUserOut}>
+                    Log Out
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </MuiContainer>
+      </AppBar>
+
+      <MuiContainer maxWidth="xl">
+        <Box pt={2}>{children}</Box>
+      </MuiContainer>
+    </>
+  );
+};
+
+export default Container;
