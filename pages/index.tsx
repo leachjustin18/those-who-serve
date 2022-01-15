d7befd53054541abb6557615c7b417c4c0186610
Justin Leach <leachjustin18@yahoo.com>
2022-01-15 14:31:50 -0600

    Remove test code from index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,22 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import { Typography } from '@mui/material';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import firebase from './firebase/clientApp';
 import LoggedInGuard from './components/authorization/LoggedInGuard';
 import Container from './components/layout/Container';
 
-const Home = () => {
-  const [servants, servantsLoading] = useCollection(
-    firebase.firestore().collection('servants'),
-    {}
-  );
+const Home = () => (
+  <>
+    <Head>
+      <title>Those who serve</title>
+    </Head>
 
-  if (!servantsLoading && servants) {
-    servants.docs.map((doc) => console.log(doc.data()));
-  }
-
-  return (
-    <>
-      <Head>
-        <title>Those who serve</title>
-      </Head>
-
-      <LoggedInGuard>
-        <Container>
-          <main>
-            <Typography variant="h2">Hello world!</Typography>
-            <Link href="/servants">Servants</Link>
-          </main>
-        </Container>
-      </LoggedInGuard>
-    </>
-  );
-};
+    <LoggedInGuard>
+      <Container>
+        <div>
+          <Typography variant="h2">Hello world!</Typography>
+        </div>
+      </Container>
+    </LoggedInGuard>
+  </>
+);
 
 export default Home;
